2ec789e60d42e9351673046e52e242bf436358bb
JulienSisi <julien.sisavath@gmail.com>
2021-10-18 23:25:57 +0200

    2.1 done

--- B/2.1/bonjour.py
+++ B/2.1/bonjour.py
@@ -3,6 +3,7 @@ print("\n")
 print("--> Manipulation de chaines de caracteres <--\n")
 bonjour = "Hello World!"
 print(bonjour)
+print("\n")
 
 # Afficher premiere lettre bonjour
 first_char = bonjour[0]
@@ -11,6 +12,7 @@ print("\n")
 
 # Compter longueur chaine de caracteres
 print(len(bonjour))
+print("\n")
 
 # Afficher derniere lettre bonjour
 last_char = bonjour[-1]
@@ -32,9 +34,11 @@ if first_sample_char == "Hel" :
   print("Vérification OK, bonjour commence bien par Hel")
 else :
   print("Vérification NOK, bonjour ne commence pas par Hel")
+print("\n")
 
 # Vérification chaine de caracteres de fin
 if sec_sample_char == "World" :
   print("Vérification OK, bonjour fini bien par World !")
 else :
   print("Vérification NOK, bonjour ne fini pas par World !")
+print("\n")
