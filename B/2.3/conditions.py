0e6c21f0c7284470d1746c6f81d1f61d2558902e
JulienSisi <julien.sisavath@gmail.com>
2021-10-18 23:27:07 +0200

    2.3 done

---
+++ B/2.3/conditions.py
@@ -0,0 +1,11 @@
+# Manipulations de conditions
+from typing import Counter
+
+
+age = input("Quel âge avez-vous ?\n")
+
+if int(age) > 17 :
+  print("Vous êtes majeur !")
+else :
+  print("Vous êtes encore mineur !")
+
